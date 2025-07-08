b2ccdd5a3248393e27d0ccecf03f2ea9cef1bbd2
Maithili-007 <patelmaithili55@gmail.com>
2025-07-08 18:15:48 +0530

    added the project

---
+++ todo backend/models/TodoItem.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const todoItemSchema = mongoose.Schema(
+  {
+  task:{
+    type:String,
+    required:true
+  },
+  date:Date,
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {Timestamp:true}
+);
+
+module.exports = mongoose.model("TodoItem",todoItemSchema);
